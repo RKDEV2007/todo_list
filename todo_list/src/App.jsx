f4265da948d2a3af4d37efa20448a4e90d4a9c0b
Ob1ch <romakarpov90@gmail.com>
2026-04-19 17:05:37 +0300

    changed css to fit todo-list finished the logic of the poject

--- todo_list/src/App.jsx
+++ todo_list/src/App.jsx
@@ -1,7 +1,19 @@
 import { useState } from 'react';
+function TodoItem({ todo, onDelete, onToggle }) {
+  return (
+    <li className="todo-item">
+      <span className="todo-text" style={{ textDecoration: todo.completed ? 'line-through' : 'none', color: todo.completed ? 'gray' : 'black' }}>{todo.text}</span>
+      <div className="todo-actions">
+        <button className="todo-button" type="button" onClick={() => onDelete(todo.id)}>Delete</button>
+        <button className="todo-button" type="button" onClick={() => onToggle(todo.id)}>{todo.completed ? 'Undo' : 'Done'}</button>
+      </div>
+    </li>
+  );
+}
 function App() {
   const [todos, setTodos] = useState([]);
   const [newTodo, setNewTodo] = useState('');
+  const isDisabled = newTodo.trim() === '';
 
   const handleAddTodo = () => {
     if (newTodo.trim() === '') return;
@@ -15,19 +27,25 @@ function App() {
     setTodos(todos.map((todo) => todo.id === id ? { ...todo, completed: !todo.completed } : todo));
   };
   return (
-    <div>
+    <div className="app">
       <h1>Todo List</h1>
-      <input type= "text" placeholder="Add a new todo" value={newTodo} onChange={(e) => setNewTodo(e.target.value)} />
-      <button onClick={handleAddTodo}>Add</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id} style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}>
-            {todo.text}
-            <button type="button" onClick={() => handleDeleteTodo(todo.id)}>Delete</button>
-            <button type="button" onClick={() => handleToggleTodo(todo.id)}>{todo.completed ? 'Not Done' : 'Done'}</button>
-          </li>
-        ))}
-      </ul>
+      <div className="todo-form">
+        <div className="todo-controls">
+          <input className="todo-input" type="text" placeholder="Add a new todo" value={newTodo} onKeyDown= {(e) => {if (e.key === 'Enter') handleAddTodo()}} onChange={(e) => setNewTodo(e.target.value)}/>
+          <button disabled={isDisabled} className={isDisabled ? 'todo-button todo-button--add todo-button--disabled': 'todo-button todo-button--add'} type="button" onClick={handleAddTodo}>
+            Add
+          </button>
+        </div>
+      </div>
+      {todos.length === 0 ? (
+  <p className="center">No todos found</p>
+) : (
+  <ul className="todo-list">
+    {todos.map((todo) => (
+      <TodoItem key={todo.id} todo={todo} onDelete={handleDeleteTodo} onToggle={handleToggleTodo} />
+    ))}
+  </ul>
+)}
     </div>
   );
 }
